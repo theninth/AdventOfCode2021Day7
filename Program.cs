165583ea74e8f84369d4b41e2ca1f01679841277
thenajnth <thenajnth@gmail.com>
2022-09-23 21:08:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TheTreacheryOfWhales;
+
+public static class Game
+{
+    private static readonly int[] Positions = {
+        16, 1, 2, 0, 4, 2, 7, 1, 2, 14
+    };
+
+    private static int CalcMovesToPos(int fromPos, int toPos) => Math.Abs(fromPos - toPos);
+
+    public static void Main()
+    {
+        Console.WriteLine(CalcMovesToPos(16, 2));
+    }
+}
