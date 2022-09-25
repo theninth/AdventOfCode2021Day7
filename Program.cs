9ed8f79642f15ae9d045a6d47d1bb34c73c6162c
thenajnth <thenajnth@gmail.com>
2022-09-25 15:27:52 +0200

    Renamed methods/variables and fixed comments to better describe what's happening in part 2

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ public static class Program
     private const string DataFile = "SampleData.txt";
 
     /// <summary>
-    /// A list where each item is a crab and the value it is initial horizontal position.
+    /// A list where each item is a crab and the value is it's initial horizontal position.
     /// </summary>
     private static readonly List<Int32> Crabs = new();
 
@@ -33,7 +33,7 @@ public static class Program
     /// <param name="fromPos">Position A</param>
     /// <param name="toPos">Position B</param>
     /// <returns>A positive (or zero) value of fuel needed for that move.</returns>
-    private static int CalcFuelToPosPart1(int fromPos, int toPos) => Math.Abs(fromPos - toPos);
+    private static int FuelCalculatorPart1(int fromPos, int toPos) => Math.Abs(fromPos - toPos);
 
     /// <summary>
     /// Calculates fuel needed to move between two positions for part 2 of assignment.
@@ -41,42 +41,44 @@ public static class Program
     /// <param name="fromPos">Position A</param>
     /// <param name="toPos">Position B</param>
     /// <returns>A positive (or zero) value of fuel needed for that move.</returns>
-    private static int CalcFuelToPosPart2(int fromPos, int toPos)
+    private static int FuelCalculatorPart2(int fromPos, int toPos)
     {
         int noOfMoves = Math.Abs(fromPos - toPos);
         return noOfMoves * (noOfMoves + 1) / 2;
     }
 
     /// <summary>
-    /// Calculate number of moves for each of the crabs to get to the right position.
+    /// Calculate fuel needed for each of the crabs to get to the right position.
     /// </summary>
     /// <param name="toPos">Target position.</param>
-    /// <param name="fuelCalculator">Algorithm to calculate fuel needed between two </param>
+    /// <param name="fuelCalculator">Algorithm to calculate fuel burnt between two positions.</param>
     /// <returns>
-    /// An array where each element is number of moves for the crab with the same index in the
-    /// Crabs-list.
+    /// An array where each element is the fuel the crab with the same index in the 
+    /// Crabs-list needs to get to target position.
     /// </returns>
-    private static int[] CalcNoOfMovesForCrabs(int toPos, FuelCalculator fuelCalculator)
+    private static int[] CalcFuelForEachCrab(int toPos, FuelCalculator fuelCalculator)
     {
         int noOfCrabs = Crabs.Count();
-        int[] noOfMoves = new int[noOfCrabs];
+        int[] fuelForEachCrab = new int[noOfCrabs];
 
         for (int crabIdx = 0; crabIdx < noOfCrabs; crabIdx++)
         {
             int fromPos = Crabs[crabIdx];
-            noOfMoves[crabIdx] = fuelCalculator(fromPos, toPos);
+            fuelForEachCrab[crabIdx] = fuelCalculator(fromPos, toPos);
         }
 
-        return noOfMoves;
+        return fuelForEachCrab;
     }
 
     /// <summary>
-    /// Calculate best position and number of moves each crab has to make.
+    /// Calculate best position and fuel needed for each crab to get there. This method is
+    /// a little complicated because it needs to retain fuel burnt for each crab to be able
+    /// to output this in verbose mode.
     /// </summary>
     /// <returns>
     /// A tuple with:
     ///   - item1: Best position to move to.
-    ///   - item2: Moves for each crab to get there.
+    ///   - item2: Fuel needed for each crab to get there.
     /// </returns>
     private static (int, int[]) CalcBestPosition(FuelCalculator fuelCalculator)
     {
@@ -84,37 +86,37 @@ public static class Program
         // lowest positioned crab to the highest positioned.
         // The array will have the form of {{a, b, c ...}, {a, b, c ...}, {a, b, c ...}}
         // where each sub array represents a position on the horizontal axis and each
-        // letter is how many moves the crab of that index needs to take.
+        // letter is fuel burnt for the crab of that index to get to that position.
         int[][] reasonablePositions = new int[Crabs.Max() - Crabs.Min() + 1][];
 
         for (int i = 0; i < reasonablePositions.Length; i++)
         {
-            reasonablePositions[i] = CalcNoOfMovesForCrabs(Crabs.Min() + i, fuelCalculator);
+            reasonablePositions[i] = CalcFuelForEachCrab(Crabs.Min() + i, fuelCalculator);
         }
 
         // Find out which of those positions have the lowest number of crab moves.
-        int BestIdx = 0;
-        int bestSum = Int32.MaxValue;
+        int bestIdx = 0;
+        int bestFuel = Int32.MaxValue;
 
         for (int i = 0; i < reasonablePositions.Length; i++)
         {
-            int totalNoOfMoves = reasonablePositions[i].Sum();
-            if (totalNoOfMoves < bestSum)
+            int totalFuel = reasonablePositions[i].Sum();
+            if (totalFuel < bestFuel)
             {
-                bestSum = totalNoOfMoves;
-                BestIdx = i;
+                bestFuel = totalFuel;
+                bestIdx = i;
             }
         }
 
-        int bestPos = Crabs.Min() + BestIdx;
-        int[] bestMoves = reasonablePositions[BestIdx];
-        return (bestPos, bestMoves);
+        int pos = Crabs.Min() + bestIdx;
+        int[] crabsFule = reasonablePositions[bestIdx];
+        return (pos, crabsFule);
     }
 
     /// <summary>
     /// Read in values from datafile supplied with assignment (might not be the same as original).
     /// </summary>
-    private static void InputData()
+    private static void ReadData()
     {
         // Really quick and ugly way to read data file.
         string textValues = File.ReadAllText(DataFile).Trim('{').Trim('}');
@@ -135,23 +137,23 @@ public static class Program
     /// <summary>
     /// Outputs data to screen.
     /// </summary>
-    /// <param name="targetPos">Target position.</param>
-    /// <param name="noOfMoves">Array with each crabs number of moves to get to target.</param>
-    /// <param name="verbose">If each move should be outputted</param>
-    private static void OutputToScreen(int targetPos, int[] noOfMoves, bool verbose=false)
+    /// <param name="pos">Target position.</param>
+    /// <param name="crabsFuel">Array with each crabs fuel needed to burn to get to target.</param>
+    /// <param name="verbose">Output debug data with each move.</param>
+    private static void OutputToScreen(int pos, int[] crabsFuel, bool verbose=false)
     {
         if (verbose)
         {
             Console.WriteLine();
-            for (int i = 0; i < noOfMoves.Length; i++)
+            for (int i = 0; i < crabsFuel.Length; i++)
             {
-                Console.WriteLine($"  - Move from {Crabs[i]} to {targetPos}: {noOfMoves[i]} fuel");
+                Console.WriteLine($"  - Move from {Crabs[i]} to {pos}: {crabsFuel[i]} fuel");
             }
         }
 
         Console.WriteLine();
-        Console.WriteLine($"Best position:      {targetPos,3}");
-        Console.WriteLine($"Total fuels needed: {noOfMoves.Sum(),3}");
+        Console.WriteLine($"Best position:      {pos,3}");
+        Console.WriteLine($"Total fuels needed: {crabsFuel.Sum(),3}");
         Console.WriteLine();
     }
 
@@ -162,9 +164,9 @@ public static class Program
     {
         bool verbose = args.Contains("--verbose");
 
-        InputData();
-        (int bestPos, int[] bestMoves) = CalcBestPosition(CalcFuelToPosPart1);
-        // (int bestPos, int[] bestMoves) = CalcBestPosition(CalcFuelToPosPart2);
+        ReadData();
+        (int bestPos, int[] bestMoves) = CalcBestPosition(FuelCalculatorPart1);
+        // (int bestPos, int[] bestMoves) = CalcBestPosition(FuelCalculatorPart2);
         OutputToScreen(bestPos, bestMoves, verbose);
 
         Console.ReadLine();
